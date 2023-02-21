18d6ac36d5b834e1c4d13084c3068eed4b43d191
Edwin574 <owinoedwin574@gmail.com>
2023-02-22 00:33:01 +0300

    done with programs part

--- src/components/Programs/Programs.js
+++ src/components/Programs/Programs.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 import "./Programs.css"
-import {programsData} from "../../data/programsData"
+import { programsData } from "../../data/programsData"
+import RightArrow from "../../assets/rightArrow.png"
 
 function Programs() {
   return (
@@ -12,7 +13,17 @@ function Programs() {
               <span>Programs</span>
               <span className='stroke-text'>to shape you</span>
           </div>
-      
+          <div className='program-categories'>
+              {programsData.map((program) => {
+                  return(
+                  <div className='category'>
+                      {program.image}
+                      <span>{program.heading}</span>
+                          <span>{program.details}</span>
+                          <div className='join-now'><span>Join Now</span><img src={RightArrow} alt="arrow illustration"/></div>
+                  </div>)
+          })}
+          </div>
     </div>
   );
 }

5f4ee9fe5f758bc2df41c1e3c895ca1cc35c5244
Edwin574 <owinoedwin574@gmail.com>
2023-02-21 06:17:56 +0300

    done with hero part

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,6 +2,10 @@ import React from "react";
 import Header from "../Header/Header";
 
 import "./Hero.css";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import Heart from "../../assets/heart.png";
+import Calories from "../../assets/calories.png";
 
 const Hero = () => {
   return (
@@ -32,25 +36,45 @@ const Hero = () => {
         {/*statistics section*/}
         <div className="figures">
           <div>
-            
             <span>+140</span> <span>expert coaches</span>
           </div>
           <div>
-           
             <span>+978</span> <span>members joined</span>
           </div>
           <div>
-            
             <span>+50</span> <span>fitness progress</span>
           </div>
         </div>
         {/*hero buttons*/}
         <div className="hero-buttons">
-        <button className="btn">Get Started</button>
-        <button className="btn">Learn More</button>
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
         </div>
       </div>
-      <div className="right-h">rigth side</div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+        <div className="heart-rate">
+          <img src={Heart} alt="this is a heart" />
+          <span>Heart Rate</span>
+          <span>116 Bpm</span>
+        </div>
+
+        {/*Hero images */}
+        <img src={hero_image} alt="a person in a gym" className="hero-image" />
+        <img
+          src={hero_image_back}
+          alt="backgroung lines for decoration"
+          className="hero-image-back"
+        />
+        {/* Calories section*/}
+        <div className="calories">
+          <img src={Calories} alt="description of calories" />
+          <div>
+            <span>Calories Burned</span>
+            <span> 220 kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

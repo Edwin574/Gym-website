ffd10ff2559a97945e5755671aa2f9fb3a14c0c6
Edwin574 <owinoedwin574@gmail.com>
2023-02-24 00:22:08 +0300

    done with plans

---
+++ src/components/Testimonials/Testimonial.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Testimonial.css'
+function Testimonial() {
+  return (
+      <div className='Testimonials'>
+          <div className='left-t'>
+          <span>Testimonials</span>
+          <span>What they</span>
+          <span>say about us</span>
+          </div>
+          <div className='right-t'></div>
+      
+    </div>
+  );
+}
+
+export default Testimonial;

a91fa03a1b31d67fcf80234e1d84d729da7aa208
Edwin574 <owinoedwin574@gmail.com>
2023-02-23 23:52:34 +0300

    working on plans

--- src/components/Plans/Plans.js
+++ src/components/Plans/Plans.js
@@ -1,20 +1,43 @@
-import React from 'react';
-import "./Plans.css"
-import {plansData} from "../../data/plansData"
+import React from "react";
+import "./Plans.css";
+import whiteTick from '../../assets/whiteTick.png'
+import { plansData } from "../../data/plansData";
 
 function Plans() {
   return (
-    <div className='plans-container'>
-      <div className='programs-header'>
-      <span className='stroke-text'>READY TO START </span>
-      <span>YOUR JOURNEY </span>
-      <span className='stroke-text'>NOW WITH US</span>
+    <div className="plans-container">
+      <div className="programs-header">
+        <span className="stroke-text">READY TO START </span>
+        <span>YOUR JOURNEY </span>
+        <span className="stroke-text">NOW WITH US</span>
       </div>
       {/*plans card*/}
-      <div className='plans'>
-      
+      <div className="plans">
+        {plansData.map((plan,i)=>{
+          return(
+            <div className="plan" key={i}>
+              {plan.icon}
+              <span>{plan.name}</span>
+              <span>{plan.price}</span>
+              <div className="features">
+                {plan.features.map((feature, i) => {
+                  
+                  return (
+                    <div className="feature">
+                      <img src={whiteTick} alt='tick illustration' />
+                      <span key={i}>{feature}</span>
+                    </div>
+                  )
+                })}
+              </div>
+              <div>
+              <span>See more Benefits -> </span>
+              </div>
+              <button className="btn">Join now</button>
+            </div>
+          )
+        }  )}
       </div>
-      
     </div>
   );
 }

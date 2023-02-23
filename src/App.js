645fb0c7ce127ef152a2b7c623755bceee5bde2b
Edwin574 <owinoedwin574@gmail.com>
2023-02-24 00:38:46 +0300

    testimonials

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Hero from './components/Hero/Hero'
 import Plans from './components/Plans/Plans';
 import Programs from './components/Programs/Programs';
 import Reasons from './components/Reasons/Reasons';
+import Testimonial from './components/Testimonials/Testimonial';
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
       <Reasons />
-      <Plans/>
+      <Plans />
+      <Testimonial/>
     </div>
   );
 }
--- src/components/Testimonials/Testimonial.js
+++ src/components/Testimonials/Testimonial.js
@@ -5,7 +5,7 @@ function Testimonial() {
       <div className='Testimonials'>
           <div className='left-t'>
           <span>Testimonials</span>
-          <span>What they</span>
+          <span className='stroke-text'>What they</span>
           <span>say about us</span>
           </div>
           <div className='right-t'></div>

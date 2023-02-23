ffeddf7c7f02fcac4dfede7811b164aa5c5e7138
Edwin574 <owinoedwin574@gmail.com>
2023-02-23 19:05:35 +0300

    working on plans

---
+++ src/components/Plans/Plans.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Plans.css"
+import {plansData} from "../../data/plansData"
+
+function Plans() {
+  return (
+    <div className='plans-container'>
+      <div className='programs-header'>
+      <span className='stroke-text'>READY TO START </span>
+      <span>YOUR JOURNEY </span>
+      <span className='stroke-text'>NOW WITH US</span>
+      </div>
+      {/*plans card*/}
+      <div className='plans'>
+      
+      </div>
+      
+    </div>
+  );
+}
+
+export default Plans;

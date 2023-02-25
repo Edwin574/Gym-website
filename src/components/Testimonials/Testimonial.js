6e8a7ded6032f2b1d24556fed38bc4637cbfce07
Edwin574 <owinoedwin574@gmail.com>
2023-02-25 22:46:51 +0300

    started testimonials

--- src/components/Testimonials/Testimonial.js
+++ src/components/Testimonials/Testimonial.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import './Testimonial.css'
+import React from "react";
+import "./Testimonial.css";
+import { testimonialsData } from "../../data/testimonialsData";
 function Testimonial() {
   return (
-      <div className='Testimonials'>
-          <div className='left-t'>
-          <span>Testimonials</span>
-          <span className='stroke-text'>What they</span>
-          <span>say about us</span>
-          </div>
-          <div className='right-t'></div>
-      
+    <div className="Testimonials">
+      <div className="left-t">
+        <span>Testimonials</span>
+        <span className="stroke-text">What they</span>
+              <span>say about us</span>
+              <span>
+              {testimonialsData[0].review}
+              </span>
+      </div>
+      <div className="right-t"></div>
     </div>
   );
 }

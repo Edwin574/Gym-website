926bc703eb06c39707f3cc35f789806023c9ef1a
Edwin574 <owinoedwin574@gmail.com>
2023-03-02 20:19:24 +0300

    done with static

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Hero from "./components/Hero/Hero";
 import Join from "./components/Join/Join";
 import Plans from "./components/Plans/Plans";
@@ -13,7 +14,8 @@ function App() {
       <Reasons />
       <Plans />
       <Testimonial />
-      <Join/>
+      <Join />
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Footer.css";
+
+import Github from "../../assets/github.png";
+import Instagram from "../../assets/instagram.png";
+import LinkedIn from "../../assets/linkedin.png";
+import Logo from "../../assets/logo.png";
+
+
+function Footer() {
+  return (
+    <div className="Footer-container">
+      <hr />
+      <div className="footer">
+        <div className="social-links">
+          <img src={Github} alt="Github" />
+          <img src={Instagram} alt="Instagram" />
+          <img src={LinkedIn} alt="Linked in" />
+       
+
+        </div>
+
+        <div className="logo-f">
+          <img src={Logo} alt="fit club logo" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
